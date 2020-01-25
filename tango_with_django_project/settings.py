bcf90b39d557fec413c9e38ab6cabf5a07028f13
Borislav Kratchanov <2401649K@student.gla.ac.uk>
2020-01-25 20:29:41 +0000

    Added support for media files (+ a cat image �)

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Build directroies used
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -127,5 +129,7 @@ STATIC_URL = '/static/'
 # Static directories 
 STATICFILES_DIRS = [STATIC_DIR, ]
 
-
+# Media directories
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
 

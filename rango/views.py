b15a4af510219b182aae4111e75abda65017494f
Borislav Kratchanov <2401649K@student.gla.ac.uk>
2020-01-31 01:04:44 +0000

    bug fixes tailored to make unit tests work

--- rango/views.py
+++ rango/views.py
@@ -6,13 +6,13 @@ from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by("-views")[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    
+
     return render(request, 'rango/index.html', context=context_dict)
 
 
@@ -29,7 +29,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = pages
         context_dict['category'] = category
 
-    except:
+    except Category.DoesNotExist:
         context_dict['category'] = None
         context_dict['pages'] = None
 

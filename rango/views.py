46aa7204f00c610e957ff6edc8415efb7ec72e1f
Borislav Kratchanov <2401649K@student.gla.ac.uk>
2020-02-12 20:12:18 +0000

    Added login page

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,7 @@ from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
@@ -125,6 +126,28 @@ def register(request):
                              'registered':registered} )
 
 
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+        return render(request, 'rango/login.html')
+    
 
 
 
